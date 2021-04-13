7a996ca69f1c73bebb718d5ff7004718908fc53b
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-13 16:01:18 -0300

    feat: add api to update property

--- src/routes/owner.ts
+++ src/routes/owner.ts
@@ -99,5 +99,84 @@ router.post("/property", async (req, res) => {
   }
 })
 
+router.patch("/:id/property", async (req, res) => {
+  // @ts-ignore
+  const userId = req.loggedUserId;
+  const id = parseInt(req.params.id)
+
+  const { name,
+    description,
+    category,
+    vacancyPrice,
+    cep,
+    street,
+    neighborhood,
+    city,
+    uf,
+    country,
+    number,
+    hasPool,
+    hasGarage,
+    hasGourmet,
+    hasInternet,
+    isPetFriendly,
+    isAdversiment
+  } = req.body as unknown as {
+    name: string,
+    description: string,
+    category: propertyCategory,
+    vacancyPrice: number,
+    cep: string,
+    street: string,
+    neighborhood: string,
+    city: string,
+    uf: string,
+    country: string,
+    number: string,
+    hasPool: boolean,
+    hasGarage: boolean,
+    hasGourmet: boolean,
+    hasInternet: boolean,
+    isPetFriendly: boolean,
+    isAdversiment: boolean
+  }
+
+  const propertyResult = await prisma.property.findFirst({
+    where: {
+      ownerId: userId
+    }
+  })
+
+  checkIfSameUser(propertyResult.ownerId, userId, res)
+
+  const result = await prisma.property.update({
+    where: {
+      id
+    },
+    data: {
+      name,
+      description,
+      category,
+      vacancyPrice,
+      cep,
+      street,
+      neighborhood,
+      city,
+      uf,
+      country,
+      number,
+      hasPool,
+      hasGarage,
+      hasGourmet,
+      hasInternet,
+      isPetFriendly,
+      isAdversiment
+    }
+  })
+
+  res.json(result)
+})
+
+
 
 export default router
