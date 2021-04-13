13f5de6ba1c251b4b03e8345fd0a75259d0d8178
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-13 15:28:40 -0300

    feat: create route get all interests from owner

--- src/routes/owner.ts
+++ src/routes/owner.ts
@@ -5,6 +5,23 @@ import { bothConfirmation, checkIfSameUser } from '../helpers/subscribers'
 const prisma = new PrismaClient()
 const router = Router()
 
+router.get("/interests", async (req, res) => {
+  // @ts-ignore
+  const propertyId = req.loggedUserId;
+
+  const result = await prisma.interest.findMany({
+    where: {
+      propertyId
+    },
+    include: {
+      Property: true,
+      User: true
+    }
+  })
+
+  res.json(result)
+})
+
 
 
 export default router
