818a4d6f0ff6e1439c7502fd4bdef8ab91963833
Nicolas <nicolasterramaia@gmail.com>
2021-04-14 08:29:13 -0300

    fix: fix cors function calling; remove wrong cors import

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,6 @@ import { bodyParser, contentType } from './middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
-  app.use(cors)
+  app.use(cors())
   app.use(contentType)
 }
