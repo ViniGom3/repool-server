b76fb3f856cbb953666be5e95437283a655f1f5b
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-16 18:43:32 -0300

    feat: configure multer to upload files for Google Cloud

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,23 +1,10 @@
 import multer from 'multer'
 import path from 'path'
-import crypto from 'crypto'
+import MulterGoogleCloudStorage from 'multer-cloud-storage';
 
 const config = {
   dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"))
-    },
-    filename: (req, file, cb) => {
-      crypto.randomBytes(16, (err, hash) => {
-        if (err) cb(err)
-
-        const filename = `${hash.toString('hex')}- ${file.originalname}`
-
-        cb(null, filename)
-      })
-    }
-  }),
+  storage: new MulterGoogleCloudStorage(),
   limits: {
     fileSize: 5 * 1024 * 1024
   },
