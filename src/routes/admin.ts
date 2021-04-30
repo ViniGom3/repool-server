a91c5e8ccdde0fbcf381ae233392a3cdb8d3deba
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-29 23:35:29 -0300

    feat: create api to mensure how many people by sex

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -7,5 +7,34 @@ import { Property } from '../classes'
 
 const router = Router()
 
+router.get('/sex', async (req, res) => {
+
+  const men = await prisma.user.count({
+    where: {
+      sex: "MALE"
+    }
+  })
+
+  const women = await prisma.user.count({
+    where: {
+      sex: "FEMALE"
+    }
+  })
+
+  const unknow = await prisma.user.count({
+    where: {
+      sex: "NOTKNOW"
+    }
+  })
+
+  const notapplicable = await prisma.user.count({
+    where: {
+      sex: 'NOTAPPLICABLE'
+    }
+  })
+
+  const countSex = { men, women, unknow, notapplicable }
+  res.json(countSex)
+})
 
 export default router
