64f30a22caba1d5f750595af6d8c3c11fe4c5a87
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-30 00:07:54 -0300

    feat: enhance api sex for mounth

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { prisma } from '../database'
+import { handleDateAgo } from '../helpers'
 
 const router = Router()
 
@@ -41,41 +42,60 @@ router.get('/sex', async (req, res) => {
 })
 
 router.get('/sex-mounth', async (req, res) => {
+  try {
+    const today = new Date()
+    const thirtyDaysAgo = handleDateAgo(today)
 
-  const today = new Date()
-  const DAYS_AGO = 30
-  const thirtyDaysAgoInSeconds = new Date().setDate(today.getDate() - DAYS_AGO)
-  const thirtyDaysAgo = new Date(thirtyDaysAgoInSeconds)
-
-  const men = await prisma.user.count({
-    where: {
-      sex: "MALE",
-      createdAt: {
-        gte: thirtyDaysAgo
+    const all = await prisma.user.count({
+      where: {
+        createdAt: {
+          gte: thirtyDaysAgo
+        }
       }
-    }
-  })
+    })
 
-  const women = await prisma.user.count({
-    where: {
-      sex: "FEMALE"
-    }
-  })
+    const men = await prisma.user.count({
+      where: {
+        sex: "MALE",
+        createdAt: {
+          gte: thirtyDaysAgo
+        }
+      }
+    })
 
-  const unknow = await prisma.user.count({
-    where: {
-      sex: "NOTKNOW"
-    }
-  })
+    const women = await prisma.user.count({
+      where: {
+        sex: "FEMALE",
+        createdAt: {
+          gte: thirtyDaysAgo
+        }
+      }
+    })
 
-  const notapplicable = await prisma.user.count({
-    where: {
-      sex: 'NOTAPPLICABLE'
-    }
-  })
+    const unknow = await prisma.user.count({
+      where: {
+        sex: "NOTKNOW",
+        createdAt: {
+          gte: thirtyDaysAgo
+        }
+      }
+    })
 
-  const countSex = { men, women, unknow, notapplicable }
-  res.json(countSex)
+    const notapplicable = await prisma.user.count({
+      where: {
+        sex: "NOTAPPLICABLE",
+        createdAt: {
+          gte: thirtyDaysAgo
+        }
+      }
+    })
+
+    const countSex = { all, men, women, unknow, notapplicable }
+    res.json(countSex)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ "error": "Houve um erro com o servidor" })
+  }
 })
 
 export default router
