b01a1a7c2f2ff20f5ed4f5204043132dd9fff013
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-29 23:58:20 -0300

    feat: create new api to count users by sex and created at

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -5,6 +5,9 @@ const router = Router()
 
 router.get('/sex', async (req, res) => {
   try {
+
+    const all = await prisma.user.count()
+
     const men = await prisma.user.count({
       where: {
         sex: "MALE"
@@ -29,12 +32,50 @@ router.get('/sex', async (req, res) => {
       }
     })
 
-    const countSex = { men, women, unknow, notapplicable }
+    const countSex = { all, men, women, unknow, notapplicable }
     res.json(countSex)
   } catch (error) {
     console.log(error)
     res.status(500).json({ "error": "Houve um erro com o servidor" })
   }
+})
+
+router.get('/sex-mounth', async (req, res) => {
+
+  const today = new Date()
+  const DAYS_AGO = 30
+  const thirtyDaysAgoInSeconds = new Date().setDate(today.getDate() - DAYS_AGO)
+  const thirtyDaysAgo = new Date(thirtyDaysAgoInSeconds)
+
+  const men = await prisma.user.count({
+    where: {
+      sex: "MALE",
+      createdAt: {
+        gte: thirtyDaysAgo
+      }
+    }
+  })
+
+  const women = await prisma.user.count({
+    where: {
+      sex: "FEMALE"
+    }
+  })
+
+  const unknow = await prisma.user.count({
+    where: {
+      sex: "NOTKNOW"
+    }
+  })
+
+  const notapplicable = await prisma.user.count({
+    where: {
+      sex: 'NOTAPPLICABLE'
+    }
+  })
+
+  const countSex = { men, women, unknow, notapplicable }
+  res.json(countSex)
 })
 
 export default router
