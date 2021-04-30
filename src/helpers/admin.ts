01b400ce343e9ed051fe3e9a4571ca8424801241
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-30 00:00:04 -0300

    fix: remove unnecessary console.log

--- src/helpers/admin.ts
+++ src/helpers/admin.ts
@@ -1,6 +1,5 @@
 export function verifyAdmin(req, res, next) {
   const rolePermited = "ADMIN"
-  console.log(req.loggedUserRole)
   if (req.loggedUserRole !== rolePermited)
     res.status(403).json({ "error": "Você não está autorizado a fazer essa operação" })
   next()
