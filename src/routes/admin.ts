329cef112cba92197bb6be0adefdcd1c2721049b
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-29 23:37:58 -0300

    feat: increase hundle exception and remove unnecessary imports

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,40 +1,40 @@
 import { Router } from 'express'
-import { PrismaPromise } from '@prisma/client'
 import { prisma } from '../database'
-import { bothConfirmation, checkIfSameUser, createJWT, handlePrice, handleValue, parseBoolean } from '../helpers'
-import { upload } from '../middlewares/multer'
-import { Property } from '../classes'
 
 const router = Router()
 
 router.get('/sex', async (req, res) => {
+  try {
+    const men = await prisma.user.count({
+      where: {
+        sex: "MALE"
+      }
+    })
 
-  const men = await prisma.user.count({
-    where: {
-      sex: "MALE"
-    }
-  })
+    const women = await prisma.user.count({
+      where: {
+        sex: "FEMALE"
+      }
+    })
 
-  const women = await prisma.user.count({
-    where: {
-      sex: "FEMALE"
-    }
-  })
+    const unknow = await prisma.user.count({
+      where: {
+        sex: "NOTKNOW"
+      }
+    })
 
-  const unknow = await prisma.user.count({
-    where: {
-      sex: "NOTKNOW"
-    }
-  })
+    const notapplicable = await prisma.user.count({
+      where: {
+        sex: 'NOTAPPLICABLE'
+      }
+    })
 
-  const notapplicable = await prisma.user.count({
-    where: {
-      sex: 'NOTAPPLICABLE'
-    }
-  })
-
-  const countSex = { men, women, unknow, notapplicable }
-  res.json(countSex)
+    const countSex = { men, women, unknow, notapplicable }
+    res.json(countSex)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ "error": "Houve um erro com o servidor" })
+  }
 })
 
 export default router
