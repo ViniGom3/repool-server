c0216899193112d29bf2890249c96ba7c5d5bec0
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-05-01 10:51:16 -0300

    refact: change queries in bothConfirmation

--- src/helpers/subscribers.ts
+++ src/helpers/subscribers.ts
@@ -1,4 +1,5 @@
 import { Interest } from '@prisma/client'
+import { Property } from '../classes'
 import { prisma } from '../database'
 
 export const checkIfSameUser = function (id, idLoggedUser, res) {
@@ -8,20 +9,20 @@ export const checkIfSameUser = function (id, idLoggedUser, res) {
 export const bothConfirmation = async function (result: Interest) {
   try {
     if (result.pConfirmation && result.uConfirmation) {
-      const resultProperty = await prisma.property.findFirst({
+      const resultProperty: Property = await prisma.property.findUnique({
         where: {
           id: result.propertyId
-        },
-        include: {
-          _count: {
-            select: {
-              rent: true
-            }
-          }
         }
       })
 
-      if (resultProperty.vacancyNumber > resultProperty._count.rent) {
+      const countActiveRentsOnProperty: number = await prisma.rent.count({
+        where: {
+          propertyId: result.propertyId,
+          isActive: true
+        }
+      })
+
+      if (resultProperty.vacancyNumber > countActiveRentsOnProperty) {
         const createRent = prisma.rent.create({
           data: {
             guest: {
