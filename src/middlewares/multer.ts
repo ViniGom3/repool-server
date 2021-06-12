caef5c78c70ad917e3c8b15cfc0ee9cbc332a68a
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-06-12 14:00:00 -0300

    feat: add timestamp to the filename avoid colision name

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,25 +1,28 @@
-import multer from 'multer'
-import path from 'path'
-import MulterGoogleCloudStorage from 'multer-cloud-storage';
+import multer from "multer";
+import path from "path";
+import crypto from "crypto";
+import MulterGoogleCloudStorage from "multer-cloud-storage";
 
 const config = {
   dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: new MulterGoogleCloudStorage(),
+  storage: new MulterGoogleCloudStorage({
+    filename: (req, file, cb) => {
+      const filename = `${Date.now()}-${file.originalname}`;
+      cb(null, filename);
+    },
+  }),
   limits: {
-    fileSize: 5 * 1024 * 1024
+    fileSize: 5 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-    const allowedMimes = [
-      'image/jpeg',
-      'image/png'
-    ]
+    const allowedMimes = ["image/jpeg", "image/png"];
 
     if (allowedMimes.includes(file.mimetype)) {
-      cb(null, true)
+      cb(null, true);
     } else {
-      cb(new Error('Invalid file type'))
+      cb(new Error("Invalid file type"));
     }
-  }
-}
+  },
+};
 
-export const upload = multer(config)
+export const upload = multer(config);
