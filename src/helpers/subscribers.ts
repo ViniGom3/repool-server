b74ca310932b70a9da009608a01730b7f479cd6d
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-06-12 19:42:59 -0300

    feat: create helper function to check if users are same

--- src/helpers/subscribers.ts
+++ src/helpers/subscribers.ts
@@ -1,60 +1,75 @@
-import { Interest, Property } from '../classes'
-import { prisma } from '../database'
+import { Interest, Property } from "../classes";
+import { prisma } from "../database";
 
 export const checkIfSameUser = function (id, idLoggedUser, res) {
-  if (id !== idLoggedUser) res.status(403).json({ "error": "Você não está autorizado a fazer essa operação" })
-}
+  if (id !== idLoggedUser)
+    res
+      .status(403)
+      .json({ error: "Você não está autorizado a fazer essa operação" });
+};
 
-export function greaterThan(biggerValue: number, smallerValue: number): boolean {
-  return biggerValue > smallerValue
+export const isSameUser = function (id, idLoggedUser): boolean {
+  return id === idLoggedUser;
+};
+
+export function greaterThan(
+  biggerValue: number,
+  smallerValue: number
+): boolean {
+  return biggerValue > smallerValue;
 }
 
 export const bothConfirmation = async function (interest: Interest) {
-  const { propertyId, userId, pConfirmation, uConfirmation } = interest
+  const { propertyId, userId, pConfirmation, uConfirmation } = interest;
 
   try {
     if (pConfirmation && uConfirmation) {
       const resultProperty: Property = await prisma.property.findUnique({
         where: {
-          id: propertyId
-        }
-      })
+          id: propertyId,
+        },
+      });
 
       const countActiveRentsOnProperty: number = await prisma.rent.count({
         where: {
           propertyId,
-          isActive: true
-        }
-      })
+          isActive: true,
+        },
+      });
 
-      if (greaterThan(resultProperty.vacancyNumber, countActiveRentsOnProperty)) {
+      if (
+        greaterThan(resultProperty.vacancyNumber, countActiveRentsOnProperty)
+      ) {
         const createRent = prisma.rent.create({
           data: {
             guest: {
               connect: {
-                id: userId
-              }
+                id: userId,
+              },
             },
             property: {
               connect: {
-                id: propertyId
-              }
-            }
-          }
-        })
+                id: propertyId,
+              },
+            },
+          },
+        });
 
         const deleteInterest = prisma.interest.deleteMany({
           where: {
-            userId
-          }
-        })
+            userId,
+          },
+        });
 
-        const transactional = await prisma.$transaction([createRent, deleteInterest])
-        return transactional
+        const transactional = await prisma.$transaction([
+          createRent,
+          deleteInterest,
+        ]);
+        return transactional;
       }
-      throw new Error()
+      throw new Error();
     }
   } catch (error) {
-    throw new Error()
+    throw new Error();
   }
-}
+};
