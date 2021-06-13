274b86d8fab5b88903294695869d9e6d89aeb661
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-06-13 13:53:45 -0300

    feat: createPropertySchema to valide property creation on Subscribers api

--- src/routes/subscriber.ts
+++ src/routes/subscriber.ts
@@ -16,6 +16,7 @@ import {
   FAILURE_MESSAGE,
   SUCCESS_CODE_ERROR,
 } from "../helpers/responses";
+import Joi from "joi";
 
 const router = Router();
 
@@ -699,6 +700,63 @@ router.post("/property", upload.array("img"), async (req, res) => {
       isAdvertisement: string;
       vacancyNumber: string;
     };
+
+    const createPropertySchema = Joi.object().keys({
+      name: Joi.string()
+        .pattern(/^[a-zà-ú ]+$/i)
+        .min(5)
+        .max(50)
+        .required(),
+      description: Joi.string(),
+      category: Joi.string().valid("HOUSE", "APARTMENT"),
+      vacancyPrice: Joi.string()
+        .pattern(/[0-9.,]+$/i)
+        .required(),
+      cep: Joi.string()
+        .pattern(/[0-9]{5}[0-9]{3}/)
+        .required(),
+      street: Joi.string()
+        .pattern(/^[a-zà-ú0-9 ,'-]+$/i)
+        .min(3)
+        .max(100)
+        .required(),
+      neighborhood: Joi.string()
+        .pattern(/^[a-zà-ú0-9 ,'-]+$/i)
+        .min(3)
+        .max(100)
+        .required(),
+      city: Joi.string()
+        .pattern(/^[a-zà-ú ]+$/i)
+        .min(3)
+        .max(50)
+        .required(),
+      uf: Joi.string().alphanum().min(2).max(5).required(),
+      country: Joi.string().alphanum().min(2).max(25).required(),
+      number: Joi.string().min(1),
+      complement: Joi.string()
+        .pattern(/^[a-zà-ú0-9 ,':-]+$/i)
+        .min(3)
+        .max(100),
+      hasPool: Joi.string().valid("true", "false"),
+      hasGarage: Joi.string().valid("true", "false"),
+      hasGourmet: Joi.string().valid("true", "false"),
+      hasInternet: Joi.string().valid("true", "false"),
+      isPetFriendly: Joi.string().valid("true", "false"),
+      isAdvertisement: Joi.string().valid("true", "false"),
+      vacancyNumber: Joi.string()
+        .pattern(/[0-9]+$/i)
+        .min(1)
+        .max(4),
+    });
+
+    const validateResult = createPropertySchema.validate(req.body);
+    const { error } = validateResult;
+
+    if (!!error) {
+      res.status(FAILURE_CODE_ERROR.BADREQUEST).json({
+        error: error.message,
+      });
+    }
 
     const pool = parseBoolean(hasPool);
     const garage = parseBoolean(hasGarage);
