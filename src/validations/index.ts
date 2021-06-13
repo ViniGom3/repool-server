2ff371351995acd42124dc5c00d4023772fcc916
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-06-13 14:34:56 -0300

    feat: create schemaValidator high order function

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,2 +1,12 @@
+import Joi from "joi";
+
 export * from "./user";
 export * from "./property";
+
+export default function schemaValidator(
+  schema: Joi.ObjectSchema<any>,
+  body: any
+) {
+  const validateResult = schema.validate(body);
+  return validateResult.error;
+}
