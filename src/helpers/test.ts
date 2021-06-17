2f66a67b7480c9ef5e69d5317eb27fbc12deeef6
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-06-16 22:01:31 -0300

    test: put tests in a describe

--- src/helpers/test.ts
+++ src/helpers/test.ts
@@ -1,22 +1,24 @@
 import { isGreaterThan } from "./subscribers";
 
-it("should isGreaterThan return true when the first argument is bigger than the second", () => {
-  const biggerValue = 9;
-  const smallerValue = 4;
+describe("isGreaterThan", () => {
+  it("should isGreaterThan return true when the first argument is bigger than the second", () => {
+    const biggerValue = 9;
+    const smallerValue = 4;
 
-  expect(isGreaterThan(biggerValue, smallerValue)).toBe(true);
-});
+    expect(isGreaterThan(biggerValue, smallerValue)).toBe(true);
+  });
 
-it("should isGreaterThan return false when the first argument is smaller than the second", () => {
-  const smallerValue = 4;
-  const biggerValue = 9;
+  it("should isGreaterThan return false when the first argument is smaller than the second", () => {
+    const smallerValue = 4;
+    const biggerValue = 9;
 
-  expect(isGreaterThan(smallerValue, biggerValue)).toBe(false);
-});
+    expect(isGreaterThan(smallerValue, biggerValue)).toBe(false);
+  });
 
-it("should isGreaterThan return false when the first argument is equal to the second", () => {
-  const smallerValue = 5;
-  const biggerValue = 5;
+  it("should isGreaterThan return false when the first argument is equal to the second", () => {
+    const smallerValue = 5;
+    const biggerValue = 5;
 
-  expect(isGreaterThan(smallerValue, biggerValue)).toBe(false);
+    expect(isGreaterThan(smallerValue, biggerValue)).toBe(false);
+  });
 });
