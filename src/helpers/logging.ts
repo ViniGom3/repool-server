49f54cf1988c27a2e251b96141e7d99498c95c99
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-06-18 18:16:02 -0300

    feat: add date to logging

--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -2,6 +2,9 @@ import { mongo } from "../database";
 
 export async function logging(error: object) {
   const db = await mongo();
-  await db.collection("errors").insertOne(error);
-  console.log(error);
+  const date = new Date().toString();
+  const datedError = { ...error, date };
+
+  await db.collection("errors").insertOne(datedError);
+  console.log(datedError);
 }
