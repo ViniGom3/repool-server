baa77b697c49e918159e4a0bf36ddbabfb021b6f
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-06-23 18:18:38 -0300

    feat: add logging to errorMiddlware

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,6 +1,7 @@
-import { FAILURE_CODE_ERROR, FAILURE_MESSAGE } from "../helpers";
+import { FAILURE_CODE_ERROR, FAILURE_MESSAGE, logging } from "../helpers";
 
 export function ErrorMiddleware(error, req, res, next) {
+    logging(error)
     const status = error.status || FAILURE_CODE_ERROR.SERVERERROR;
     const response = error.response || FAILURE_MESSAGE.SERVERERROR;
     res.status(status).json(response);
